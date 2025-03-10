2b2384cd003b959b15330ce88c7fa159a1c70bba
siesto1elemento <rohitojha9720@gmail.com>
2025-03-11 01:13:46 +0530

    small bug (loading apps before importing models)

--- poker/asgi.py
+++ poker/asgi.py
@@ -8,13 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poker.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from Game import routing
 from Game.middleware import JWTAuthMiddleware
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poker.settings")
+
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
